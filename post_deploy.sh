bd3b63966a2cde3ab413dd5cad2693baca385daf
maxverstappen583 <maharajbhawanslifestyle@gmail.com>
2025-09-08 20:17:45 +0530

    Update post_deploy.sh

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Upgrade pip and force reinstall dependencies
 python3 -m pip install --upgrade pip
 python3 -m pip install --force-reinstall -r requirements.txt
-
-# Start the bot
 python3 bot.py
